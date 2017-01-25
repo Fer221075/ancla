50793f77931f00470c3f399265c01dd9bcbfc713
David Paternina <David Paternina>
2017-01-25 13:27:04 -0500

    Price format fix

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -14,8 +14,8 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 
 <div class="col-md-3">
     <article <?php post_class('prod'); ?>>
-        <a href="<?php the_permalink(); ?>"><?php echo woocommerce_get_product_thumbnail(); ?></a>
-        <a href="<?php the_permalink(); ?>"><h4 class="product-name"><?php the_title(); ?></h4></a>
+        <a href="#<?php //the_permalink(); ?>"><?php echo woocommerce_get_product_thumbnail(); ?></a>
+        <a href="#<?php //the_permalink(); ?>"><h4 class="product-name"><?php the_title(); ?></h4></a>
         <span class="price"><?php echo wc_price($product->get_price()); //echo $product->get_price(); ?></span>
 <!--        <a href="#" class="add-to-cart">Agregar al carrito <i class="fa fa-shopping-bag" aria-hidden="true"></i></a>-->
     </article>
