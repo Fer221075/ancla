8603d203ba2dfc97f7976339eca6e2245ce7beff
David Paternina <david@fanxielab.com>
2017-02-01 15:44:20 -0500

    WP Nosotros

--- page-nosotros.php
+++ page-nosotros.php
@@ -1,22 +1,21 @@
 <?php
+
+/* Template Name: Template Nosotros */
+
 get_header(); ?>
 
 <section class="cover-slides">
-    <div class="cover slide bg-img">
-        <img src="<?php echo get_template_directory_uri();?>/img/slide_gallery1.jpg" alt="soporte">
-    </div>
-    <div class="cover slide bg-img">
-        <img src="<?php echo get_template_directory_uri();?>/img/slide_gallery2.jpg" alt="soporte">
-    </div>
-    <div class="cover slide bg-img">
-        <img src="<?php echo get_template_directory_uri();?>/img/slide_gallery3.jpg" alt="soporte">
-    </div>
-    <div class="cover slide bg-img">
-        <img src="<?php echo get_template_directory_uri();?>/img/slide_gallery4.jpg" alt="soporte">
-    </div>
-    <div class="cover slide bg-img"><div>
-        <img src="<?php echo get_template_directory_uri();?>/img/slide_gallery5.jpg" alt="soporte">
-    </div>
+
+    <?php
+    $gallery = CFS()->get( 'gallery' );
+
+    foreach ($gallery as $image): ?>
+
+        <div class="cover slide bg-img">
+            <img src="<?php echo $image['image']?>" alt="soporte">
+        </div>
+
+    <?php endforeach; ?>
 </section>
 
 <div class="red-section-header no-triangle">
@@ -25,52 +24,51 @@ get_header(); ?>
     </div>
 </div>
 
+<?php
+
+$loop = CFS()->get( 'tabs' );
+$first = true;
+?>
+
 <section class="container">
     <nav class="row tab-anchors">
         <ul class="clearfix">
-            <li>
-                <a href="#mision" class="tab-anchor smooth-anchor current">Misión</a>
-            </li>
-            <li>
-                <a href="#vision" class="tab-anchor smooth-anchor">Visión</a>
-            </li>
-            <li>
-                <a href="#historia" class="tab-anchor smooth-anchor">Historia</a>
-            </li>
-            <li>
-                <a href="#respaldo" class="tab-anchor smooth-anchor">Nuestro respaldo</a>
-            </li>
+            <?php
+            foreach ( $loop as $row ) {
+
+                $anchor = strtolower(preg_replace('/[^a-zA-Z0-9-_\.]/','', $row['tab_title']));
+
+                ?>
+
+                <li>
+                    <a href="#<?php echo $anchor ?>" class="tab-anchor smooth-anchor <?php if ( $first ): ?>current<?php endif; ?>"><?php echo $row['tab_title'] ?></a>
+                </li>
+
+                <?php
+                $first = false;
+            }
+            ?>
         </ul>
     </nav>
 
-    <section id="mision" class="row us-section">
-            <h3>Misión</h3>
-            <p>Cajas Fuertes Ancla S.A. fabrica y comercializa equipos; proporciona asesoría y soluciones en seguridad física y electrónica para el sector financiero, residencial y empresarial con responsabilidad social y sostenibilidad.</p>
-    </section>
-    <section id="vision" class="row us-section">
-        <h3>Visión</h3>
-        <p>Buscamos ser competitivos en el mercado de la seguridad física y electrónica en el apoyo y aprendizaje continuo de nuestro personal; la tecnología, el cumplimiento y apertura de nuevos mercados son aliados seguros para nuestros clientes.</p>
-    </section>
-    <section id="historia" class="row us-section">
-        <h3>Historia</h3>
-        <p>Cajas Fuertes Ancla S.A. inicia labores el 8 de Octubre de 1964, en la ciudad de Medellín bajo la razón social JAVI SAFE LTDA. Posteriormente la empresa se traslada a la ciudad de Bogotá para convertirse en 1995 en la sociedad Cajas Fuertes Ancla S.A. y después de un proceso de transformación y cobertura del mercado, se estructura en una completa planta de producción, para abastecer el mercado colombiano y atender la demanda de otros países.</p>
-        <p>Es así como en la actualidad contamos con un personal idóneo y una infraestructura técnica, administrativa y comercial que nos acredita como empresa líder en el mercado de la seguridad, con el aval de una amplia red de clientes a nivel nacional e Internacional.</p>
-    </section>
-    <section id="respaldo" class="row us-section">
-        <h3>Nuestro respaldo</h3>
-        <p>Contamos con diferentes certificaciones de calidad que avalan nuestros procesos y productos.</p>
-        <ul>
-            <li><img src="<?php echo get_template_directory_uri();?>/img/iso_9001.png" alt="iso_9001"></li>
-            <li><img src="<?php echo get_template_directory_uri();?>/img/iso_14001.png" alt="iso_14001"></li>
-            <li><img src="<?php echo get_template_directory_uri();?>/img/oshsas_18001.png" alt="oshsas_18001"></li>
-        </ul>
-    </section>
+    <?php
+    foreach ( $loop as $row ) {
+
+        $anchor = strtolower(preg_replace('/[^a-zA-Z0-9-_\.]/','', $row['tab_title']));
+
+        ?>
+
+        <section id="<?php echo $anchor ?>" class="row us-section">
+                <h3><?php echo $row['tab_title'] ?></h3>
+                <?php echo $row['tab_content'] ?>
+        </section>
+
+        <?php
+        $first = false;
+    }
+    ?>
 
 </section>
-
-
-
-
 
 <?php
 get_footer();
