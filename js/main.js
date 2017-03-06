614d7a1e779e8cd935cdd8cd4acc04d36da993f1
David Paternina <david@fanxielab.com>
2017-03-06 11:11:00 -0500

    Dynamic form subject

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
+var QueryString = function () {
+    // This function is anonymous, is executed immediately and
+    // the return value is assigned to QueryString!
+    var query_string = {};
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+        var pair = vars[i].split("=");
+        // If first entry with this name
+        if (typeof query_string[pair[0]] === "undefined") {
+            query_string[pair[0]] = decodeURIComponent(pair[1]);
+            // If second entry with this name
+        } else if (typeof query_string[pair[0]] === "string") {
+            var arr = [ query_string[pair[0]],decodeURIComponent(pair[1]) ];
+            query_string[pair[0]] = arr;
+            // If third or later entry with this name
+        } else {
+            query_string[pair[0]].push(decodeURIComponent(pair[1]));
+        }
+    }
+    return query_string;
+}();
+
 /**
  * Created by David on 19/01/2017.
  */
@@ -264,6 +287,8 @@
            })
         });
 
+        formSubjectFromParam();
+
     });
 }));
 
@@ -274,6 +299,17 @@ $(window).on("load", function() {
     }, 200);
 
 });
+
+function formSubjectFromParam() {
+    if ( QueryString.asunto ) {
+        $('form #ccf_field_asunto option').each(function () {
+            if ( $(this).val().toLowerCase() == QueryString.asunto.toLowerCase() ) {
+                $(this).attr('selected', true);
+                return;
+            }
+        });
+    }
+}
 
 function showFloatingCart() {
     $('.floating-cart').slideDown();
