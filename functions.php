d1094a95af00b1b11ed264882b49744457fb2421
Jairo Andrés Mendieta Reyes <jairo@fanxielab.com>
2017-05-28 23:14:42 -0500

    Add checkbox on checkout
    Add checkbox on checkout

--- functions.php
+++ functions.php
@@ -170,6 +170,45 @@ function woocommerce_support() {
     add_theme_support( 'woocommerce' );
 }
 
+add_action('woocommerce_after_order_notes', 'my_custom_checkout_field');
+ 
+function my_custom_checkout_field( $checkout ) {
+ 
+    echo '<div id="my-new-field"><h3>'.__('My Checkbox: ').'</h3>';
+ 
+    woocommerce_form_field( 'my_checkbox', array(
+        'type'          => 'checkbox',
+        'class'         => array('input-checkbox'),
+        'label'         => __('I have read and agreed.'),
+        'required'  => true,
+        ), $checkout->get_value( 'my_checkbox' ));
+ 
+    echo '</div>';
+}
+
+/**
+ * Process the checkout
+ **/
+add_action('woocommerce_checkout_process', 'my_custom_checkout_field_process');
+ 
+function my_custom_checkout_field_process() {
+    global $woocommerce;
+ 
+    // Check if set, if its not set add an error.
+    if (!$_POST['my_checkbox'])
+         $woocommerce->add_error( __('Please agree to my checkbox.') );
+}
+ 
+/**
+ * Update the order meta with field value
+ **/
+add_action('woocommerce_checkout_update_order_meta', 'my_custom_checkout_field_update_order_meta');
+ 
+function my_custom_checkout_field_update_order_meta( $order_id ) {
+    if ($_POST['my_checkbox']) update_post_meta( $order_id, 'My Checkbox', esc_attr($_POST['my_checkbox']));
+}
+
+
 // Display empty product categories
 add_filter( 'woocommerce_product_subcategories_hide_empty', 'show_empty_categories', 10, 1 );
 function show_empty_categories ( $show_empty ) {
