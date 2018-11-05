e3ecdb0a885994ba40ceac4d9221fadbf58070b6
Juan Ramos <juan2ramos@gmail.com>
2018-11-05 18:18:31 -0500

    new sidebar news

--- functions.php
+++ functions.php
@@ -96,6 +96,15 @@ function ancla_widgets_init() {
         'after_title'   => '</h2>',
     ) );
     register_sidebar( array(
+        'name'          => esc_html__( 'Blog sidebar-news', 'ancla' ),
+        'id'            => 'sidebar-news',
+        'description'   => esc_html__( 'Add widgets here.', 'ancla' ),
+        'before_widget' => '<section id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</section>',
+        'before_title'  => '<h2 class="widget-title">',
+        'after_title'   => '</h2>',
+    ) );
+    register_sidebar( array(
         'name'          => esc_html__( 'Blog sidebar', 'ancla' ),
         'id'            => 'sidebar-blog',
         'description'   => esc_html__( 'Add widgets here.', 'ancla' ),
