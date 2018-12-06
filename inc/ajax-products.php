995fd8e99bfde6b2049456a19a9cd2fe6db16464
juan2ramos <juan2ramos@gmail.com>
2018-12-06 10:31:41 -0500

    por

--- inc/ajax-products.php
+++ inc/ajax-products.php
@@ -3,7 +3,8 @@
 add_action("wp_ajax_nopriv_get_products", "get_products");
 add_action("wp_ajax_get_products", "get_products");
 
-function get_products() {
+function get_products()
+{
 
     $filters = explode('/', $_REQUEST["filters"]);
 
@@ -17,23 +18,32 @@ function get_products() {
         $value = $filter[1];
 
         $filter_array = array(
-            'taxonomy'      => 'pa_' . $attribute,
-            'terms'         => $value,
-            'field'         => 'slug',
+            'taxonomy' => 'pa_' . $attribute,
+            'terms' => $value,
+            'field' => 'slug',
         );
 
         array_push($tax_query, $filter_array);
     }
 
-    $loop = new WP_Query( array(
+    $loop = new WP_Query(array(
         'post_type' => 'product',
         'tax_query' => $tax_query
-    ) );
-    if ( $loop->have_posts() ) :
-        while ( $loop->have_posts() ) : $loop->the_post();
+    ));
+    if ($loop->have_posts()) :
+        while ($loop->have_posts()) : $loop->the_post();
             global $product; ?>
             <div class="col-xs-6 col-sm-6 col-md-3">
-                <article <?php post_class('prod'); ?>>
+                <article <?php post_class('prod'); ?>
+                    <?php $por = '0';
+                    if (get_the_title() == '') {
+                        $por = '10%';
+                    } else {
+                        $por = '50%';
+                    } ?>
+                        data-por="<?php echo $por ?> "
+                        data-name="<?php echo get_the_title() ?> "
+                >
                     <a href="<?php the_permalink(); ?>"><?php echo woocommerce_get_product_thumbnail(); ?></a>
                     <a href="<?php the_permalink(); ?>"><h4 class="product-name"><?php the_title(); ?></h4></a>
                     <span class="price"><?php echo wc_price($product->get_price()); ?></span>
--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -6,7 +6,7 @@
  * Time: 16:05
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
 }
 
@@ -18,9 +18,9 @@ if ( ! defined( 'ABSPATH' ) ) {
  *
  * @hooked wc_print_notices - 10
  */
-do_action( 'woocommerce_before_single_product' );
+do_action('woocommerce_before_single_product');
 
-if ( post_password_required() ) {
+if (post_password_required()) {
     echo get_the_password_form();
     return;
 }
@@ -28,53 +28,54 @@ if ( post_password_required() ) {
 global $post, $product, $woocommerce;
 ?>
 
-<div itemscope itemtype="<?php echo woocommerce_get_product_schema(); ?>" id="product-<?php the_ID(); ?>" <?php post_class('row'); ?>>
+<div itemscope itemtype="<?php echo woocommerce_get_product_schema(); ?>"
+     id="product-<?php the_ID(); ?>" <?php post_class('row'); ?>>
 
     <div class="col-md-6 images">
 
-            <?php
+        <?php
 
-            $gallery = $product->get_gallery_attachment_ids();
+        $gallery = $product->get_gallery_attachment_ids();
 
-            if ( $gallery || has_post_thumbnail() ) {
-
-                ?>
-                <div class="product-main-gallery">
-                    <?php
-
-                    $image = get_the_post_thumbnail( $post->ID, apply_filters( 'single_product_large_thumbnail_size', 'shop_single' ) ) ?: '' ;
-
-                    echo $image;
-
-                    foreach ( $gallery as $attachment_id ) {
-
-                        $props = wc_get_product_attachment_props( $attachment_id, $post );
-                        echo wp_get_attachment_image( $attachment_id, array('540','540'), 0, $props );
-                    }
-
-                    ?>
-                </div>
-
-                <div class="product-thumbnails">
-                    <?php
-
-                    echo $image;
-
-                    foreach ( $gallery as $attachment_id ) {
-
-                        $props = wc_get_product_attachment_props( $attachment_id, $post );
-                        echo wp_get_attachment_image( $attachment_id, apply_filters( 'single_product_small_thumbnail_size', 'shop_thumbnail' ), 0, $props );
-                    }
-
-                    ?>
-                </div>
-                <?php
-
-            } else {
-                echo apply_filters( 'woocommerce_single_product_image_html', sprintf( '<img src="%s" alt="%s" />', wc_placeholder_img_src(), __( 'Placeholder', 'woocommerce' ) ), $post->ID );
-            }
+        if ($gallery || has_post_thumbnail()) {
 
             ?>
+            <div class="product-main-gallery">
+                <?php
+
+                $image = get_the_post_thumbnail($post->ID, apply_filters('single_product_large_thumbnail_size', 'shop_single')) ?: '';
+
+                echo $image;
+
+                foreach ($gallery as $attachment_id) {
+
+                    $props = wc_get_product_attachment_props($attachment_id, $post);
+                    echo wp_get_attachment_image($attachment_id, array('540', '540'), 0, $props);
+                }
+
+                ?>
+            </div>
+
+            <div class="product-thumbnails">
+                <?php
+
+                echo $image;
+
+                foreach ($gallery as $attachment_id) {
+
+                    $props = wc_get_product_attachment_props($attachment_id, $post);
+                    echo wp_get_attachment_image($attachment_id, apply_filters('single_product_small_thumbnail_size', 'shop_thumbnail'), 0, $props);
+                }
+
+                ?>
+            </div>
+            <?php
+
+        } else {
+            echo apply_filters('woocommerce_single_product_image_html', sprintf('<img src="%s" alt="%s" />', wc_placeholder_img_src(), __('Placeholder', 'woocommerce')), $post->ID);
+        }
+
+        ?>
     </div>
 
 
@@ -91,8 +92,8 @@ global $post, $product, $woocommerce;
          * @hooked woocommerce_template_single_meta - 40
          * @hooked woocommerce_template_single_sharing - 50
          */
-//        do_action( 'woocommerce_single_product_summary' );
-        the_title( '<h1 itemprop="name" class="prod-title">', '</h1>' );
+        //        do_action( 'woocommerce_single_product_summary' );
+        the_title('<h1 itemprop="name" class="prod-title">', '</h1>');
         ?>
 
         <p><?php echo $post->post_excerpt; ?></p>
@@ -103,14 +104,15 @@ global $post, $product, $woocommerce;
 
             <p class="price"><?php echo $product->get_price_html(); ?></p>
 
-            <meta itemprop="price" content="<?php echo esc_attr( $product->get_display_price() ); ?>" />
-            <meta itemprop="priceCurrency" content="<?php echo esc_attr( get_woocommerce_currency() ); ?>" />
-            <link itemprop="availability" href="http://schema.org/<?php echo $product->is_in_stock() ? 'InStock' : 'OutOfStock'; ?>" />
+            <meta itemprop="price" content="<?php echo esc_attr($product->get_display_price()); ?>"/>
+            <meta itemprop="priceCurrency" content="<?php echo esc_attr(get_woocommerce_currency()); ?>"/>
+            <link itemprop="availability"
+                  href="http://schema.org/<?php echo $product->is_in_stock() ? 'InStock' : 'OutOfStock'; ?>"/>
 
         </div>
-
+        <?php print_r($product->product_type) ?>
         <?php
-        do_action( 'woocommerce_' . $product->product_type . '_add_to_cart' );
+        do_action('woocommerce_' . $product->product_type . '_add_to_cart');
         ?>
 
     </div><!-- .summary -->
@@ -118,10 +120,10 @@ global $post, $product, $woocommerce;
         <h3>Descripción</h3>
         <?php the_content(); ?>
     </div>
-    <meta itemprop="url" content="<?php the_permalink(); ?>" />
+    <meta itemprop="url" content="<?php the_permalink(); ?>"/>
 
 </div><!-- #product-<?php the_ID(); ?> -->
 
 <?php woocommerce_output_related_products(); ?>
 
-<?php do_action( 'woocommerce_after_single_product' ); ?>
+<?php do_action('woocommerce_after_single_product'); ?>
