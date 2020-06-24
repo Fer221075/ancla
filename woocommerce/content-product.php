c1acbd1973ce75e6b8707f67a4bad4cd8c1527a6
David A Lopez <david.cadc@gmail.com>
2020-06-24 16:46:25 -0500

    %2

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -27,7 +27,7 @@ if (empty($product) || !$product->is_visible()) {
             }
             
             if (get_the_ID() == 41) {
-                $por = '30%';
+                $por = '10%';
             }
            
         }
